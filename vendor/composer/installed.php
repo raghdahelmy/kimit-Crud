48f7344264be74d8d93d93913b8df29b3db0fdde
Raghda Helmy <raghda.helmy82@gmail.com>
2025-01-09 23:29:04 +0200

    generating autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'raghda/crud',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '91a063d2861a6d1d369e5a4aebdc32bbfd2d134d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'raghda/crud' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '91a063d2861a6d1d369e5a4aebdc32bbfd2d134d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
